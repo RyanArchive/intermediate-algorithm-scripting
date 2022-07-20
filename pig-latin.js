f5e0d6016fca042559ef33caaf94bc98a542ba29
Ryan Aristosa <ryanaristosa@outlook.com>
2022-07-20 10:36:21 +0800

    Add pig-latin.js

---
+++ pig-latin.js
@@ -0,0 +1,26 @@
+/*
+    Pig Latin
+
+    Pig Latin is a way of altering English Words. The rules are as 
+    follows:
+        - If a word begins with a consonant, take the first consonant or 
+        consonant cluster, move it to the end of the word, and add ay to it.
+        - If a word begins with a vowel, just add way at the end.
+
+    Translate the provided string to Pig Latin. Input strings are 
+    guaranteed to be English words in all lowercase.
+*/
+
+function translatePigLatin(str) {
+    let beginning = str.match(/^[^aeiou]*/)[0];
+  
+    if (beginning === "") {
+        str += "way";
+    } else {
+        str = str.replace(beginning, "") + beginning + "ay";
+    }
+  
+    return str;
+}
+
+translatePigLatin("consonant");
